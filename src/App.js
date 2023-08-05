48fd182425b5bc8c2c03eca8b92c11d68b59db92
maxinunezz <maxicbapl@gmail.com>
2023-08-05 17:00:55 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/MyPortfolio" element={<Layout />} />
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
